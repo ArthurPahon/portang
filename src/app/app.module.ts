dd3aa4e74ec8cedf78442cf4bf9575c54d6993c9
Arthur <arthur.pahon@epitech.eu>
2022-07-25 16:28:15 +0200

    events recents finis et audits faits

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './features/home/home.component';
+import { CompetencesComponent } from './features/competences/competences.component';
+import { ProjetsComponent } from './features/projets/projets.component';
+import { ContactComponent } from './features/contact/contact.component';
+import { NavComponent } from './features/nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CompetencesComponent,
+    ProjetsComponent,
+    ContactComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
